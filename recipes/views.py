87abf01c8a602f220527bea394ffdce40773397b
Eduardo Araujo Da Silva <edu1702ardo@gmail.com>
2024-10-28 14:35:50 -0300

    Creating template inheritance

--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html')
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html')
